d2df9c2a2616c7f5a732812e3398f2f78af2c3d3
Manoj SInnur <manoj19.sinnur@gmail.com>
2024-03-04 02:01:34 +0530

    Search Logic Implemented

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.members, name="Homepage")
+    path("", views.home, name="Homepage"),
+    path("search", views.search, name="Search"),
+    path("track/<str:id>", views.track, name="Track")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import requests
 
-def members(request):
-    return HttpResponse("Hello world!")
+def home(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
+@csrf_exempt
+def search(request):
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        url = "https://accounts.spotify.com/api/token"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+                "grant_type": "client_credentials",
+                "client_id": "08de4eaf71904d1b95254fab3015d711",
+                "client_secret": "622b4fbad33947c59b95a6ae607de11d"
+               }
+        response = requests.post(url, headers=headers, data=data)
+        token = response.json()['access_token']
+        list = requests.get(f"https://api.spotify.com/v1/search?q={query}&type=track&limit=5", headers={'Authorization': 'Bearer '+ token})
+        print(list.json())
+        return JsonResponse({'searchResults': list.json()})
+    else:
+        return JsonResponse({"message": "failed"})
 
-# Create your views here.
+def track(request, id):
+    return JsonResponse({"idsdfds": id})
