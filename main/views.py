b90115f35eeff9a9e8a4eeb5905eeeb0f9aa059c
Manoj SInnur <manoj19.sinnur@gmail.com>
2024-03-05 00:28:06 +0530

    Track Page Created

--- main/views.py
+++ main/views.py
@@ -12,6 +12,24 @@ def home(request):
 def search(request):
     if request.method == 'POST':
         query = request.POST.get('query')
+        token = getSpotifyAccessToken()
+        list = requests.get(f"https://api.spotify.com/v1/search?q={query}&type=track&limit=5", headers={'Authorization': 'Bearer '+ token})
+        print(list.json())
+        return JsonResponse({'searchResults': list.json()})
+    else:
+        return JsonResponse({"message": "failed"})
+
+def track(request, id):
+    token = getSpotifyAccessToken()
+    url = f"https://api.spotify.com/v1/tracks/{id}"
+    headers = {'Authorization' : 'Bearer '+ token }
+    response = requests.get(url, headers=headers)
+    context = response.json()
+    print(context['name'])
+    template = loader.get_template('track.html')
+    return HttpResponse(template.render({'context': context}, request))
+
+def getSpotifyAccessToken():
         url = "https://accounts.spotify.com/api/token"
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         data = {
@@ -21,11 +39,4 @@ def search(request):
                }
         response = requests.post(url, headers=headers, data=data)
         token = response.json()['access_token']
-        list = requests.get(f"https://api.spotify.com/v1/search?q={query}&type=track&limit=5", headers={'Authorization': 'Bearer '+ token})
-        print(list.json())
-        return JsonResponse({'searchResults': list.json()})
-    else:
-        return JsonResponse({"message": "failed"})
-
-def track(request, id):
-    return JsonResponse({"idsdfds": id})
+        return token
