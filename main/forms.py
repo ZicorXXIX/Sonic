7d5290540a9ce4840fb70effe205fe2c089c2945
dronzzz <mayank25rathod@gmail.com>
2024-03-30 04:34:16 +0530

    added autentication

---
+++ main/forms.py
@@ -0,0 +1,11 @@
+# from .forms import RegisterForm
+
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
